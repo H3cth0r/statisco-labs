142c801adb27699b0c3a50d45de6c15cc0fab5ce
h3cth0r <hectorsucre13@gmail.com>
2024-11-07 00:04:37 -0600

    able to allocate the values

--- py_alloc/main_2.py
+++ py_alloc/main_2.py
@@ -1,6 +1,14 @@
 from source_code import dtypes
 from ops_clang import Buffer
+import struct
 
 if __name__ == "__main__":
     DEVICE = "CLANG"
+
+    # allocate some buffers and load in values
     out = Buffer(DEVICE, 1, dtypes.int32).allocate()
+    a = Buffer(DEVICE, 1, dtypes.int32).allocate().copyin(memoryview(bytearray(struct.pack("I", 2))))
+    b = Buffer(DEVICE, 1, dtypes.int32).allocate().copyin(memoryview(bytearray(struct.pack("I", 3))))
+
+    # describe the computation
+    print("finish")
