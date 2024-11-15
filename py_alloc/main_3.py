1a171df50ea25377855b98757cb74782748bbc58
h3cth0r <hectorsucre13@gmail.com>
2024-11-14 23:49:35 -0600

    added more benchmarking functions

--- py_alloc/main_3.py
+++ py_alloc/main_3.py
@@ -1,27 +1,13 @@
-from source_code import MallocAllocator
-import time
-import numpy as np
+from setuptools import setup, Extension
 
-def timer(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()  # Start the timer
-        result = func(*args, **kwargs)  # Call the function
-        end_time = time.time()  # End the timer
-        duration = end_time - start_time  # Calculate duration
-        print(f"Function '{func.__name__}' took {duration:.8f} seconds to complete.")
-        return result
-    return wrapper
+malloc_allocator_module = Extension(
+    'malloc_allocator',
+    sources=['malloc_allocator.c'],
+)
 
-@timer
-def test_allocation():
-    a = MallocAllocator.alloc(4)
-    MallocAllocator.copyin(a, memoryview(bytearray([2, 0, 0, 0])))
-@timer
-def test_np_allocation():
-    a = np.array([2, 0, 0, 0])
-
-if __name__ == "__main__":
-    print("asd")
-    test_allocation()
-    test_np_allocation()
-    test_allocation()
+setup(
+    name='malloc_allocator',
+    version='1.0',
+    description='MallocAllocator extension module',
+    ext_modules=[malloc_allocator_module],
+)
